b41e881678182e00e5c4aaf2df2a21b0b0de739d
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-05-09 16:16:13 +0900

    modify: extractor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{server, App, HttpRequest, Responder, Error, FromRequest, Path};
+use serde_derive::*;
+
+// Deserializeにしたがって抽出されるので型を用意しておく
+#[derive(Deserialize)]
+struct HelloPath {
+    // {name}に対応するフィールドを定義する
+    name: String,
+}
+
+fn hello_name(to: Path<HelloPath>) -> impl Responder {
+    format!("Hello {}!", &to.name)
+}
+//fn hello_name(req: &HttpRequest) -> Result<String, Error> {
+    // FromRequest::extractでデータを抽出する
+//    let to = Path::<HelloPath>::extract(req);
+    // Path<T>はDeref<Target=T>を実装しているのでそのままHelloPathのように扱える
+//    Ok(format!("Hello {}!", &to.name))
+//}
+
 
 fn hello(req: &HttpRequest) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
@@ -9,7 +28,8 @@ fn main() {
     server::new(|| {
         App::new()
             .resource("/", |r| r.f(hello))
-            .resource("/{name}", |r| r.f(hello))
+//            .resource("/{name}", |r| r.f(hello))
+            .resource("/{name}", |r| r.with(hello_name))
     })
         .bind("localhost:5000")
         .expect("Can not bind to port 5000")
